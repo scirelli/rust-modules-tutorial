99f1f848cfec727fb7c7e6ae1bf9b494a50237a7
Steve Cirelli <scirelli+git@gmail.com>
2025-08-02 20:01:57 -0400

    quick save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod steve {
+    pub mod vegetables {
+        pub struct Asparagus {
+            pub name: String,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let veg = crate::steve::vegetables::Asparagus{name: String::from("My butt")};
+    let veg_name = &veg.name[..];
+    println!("Hello, world!\nI have asparagus comming out of {veg_name}!!");
+    let k = &veg.name;
+    println!("Hello, world!\nI have asparagus comming out of {k}!!");
+
+    let j = veg.name;
+    println!("Hello, world!\nI have asparagus comming out of {j}!!");
 }
